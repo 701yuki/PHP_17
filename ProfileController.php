ce0ca929fff921b94d8fa69ede7dc34aa01a029b
701yuki <draw-the-line-701@ezweb.ne.jp>
2019-03-01 13:56:51 +0900

    second commit

--- ProfileController.php
+++ ProfileController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
+
 use App\Profile;
 
 class ProfileController extends Controller
@@ -32,13 +32,51 @@ class ProfileController extends Controller
        return redirect('admin/profile/create');
     }
 
-    public function edit()
+
+    public function index(Request $request)
     {
-      return view('admin.profile.edit');
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+        $posts = Profile::where('title', $cond_title)->get();
+      } else {
+
+        // dd($cond_title != '');
+
+        $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_title'
+    => $cond_title]);
     }
-    public function update()
+
+    public function edit(Request $request)
     {
-      return redirect('admin/profile/edit');
+
+      $profile = Profile::find($request->id);
+
+      return view('admin.profile.edit',['profile_form' => $profile]);
+    }
+
+    public function update(Request $request)
+    {
+      $this->validate($request, Profile::$reles);
+      $news = Profile::find($request->id);
+      $profile_form = $request->all();
+      unset($profile_form['_token']);
+      $profile->fill($profile_form)->save();
+
+      $profile->fill($profile_form)->save();
+
+      return redirect('admin/profile/');
+    }
+
+    public function delete(Request $request)
+    {
+
+      $profile = Profile::find($request->id);
+
+      $profile->delete();
+      return redirect('admin/profile/');
+
     }
 
 }
